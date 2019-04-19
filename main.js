8175c25a68c46c9e2ccc4bd80af806d9245a2576
akhileshkr797 <akhileshkr797@gmail.com>
2019-04-19 16:18:43 +0530

    created splashWindow

--- main.js
+++ main.js
@@ -24,3 +24,42 @@ function createWindow() {
         mainWindow = null
     })
 }
+
+//splash window
+function createSplashWindow() {
+    splashWindow = new BrowserWindow({
+        show: false,
+        width: 350,
+        height: 250,
+        frame: false,
+        alwaysOnTop: true,
+    })
+
+    splashWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'splash.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    splashWindow.once('ready-to-show', () => {
+        splashWindow.show()
+        createWindow()
+    })
+
+    splashWindow.on('closed', function() {
+        splashWindow = null
+    })
+}
+
+ipcMain.on('app-init', event => {
+    if (splashWindow) {
+        setTimeout(() => {
+            splashWindow.close()
+        }, 2000)
+    }
+    mainWindow.show()
+})
+
+app.on('ready', () => {
+    createSplashWindow()
+})
